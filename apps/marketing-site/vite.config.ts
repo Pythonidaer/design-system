fc6d8b2cbf45dbc4fa11fdd78797c37ba1f61c19
Johnny Hammond <30871397+Pythonidaer@users.noreply.github.com>
2026-05-29 15:06:14 -0400

    github workflow. will need to make this an npm thing in the future. probably will do backwards compatible, see what examples I can get online and generate components that way, then change the components to work with the websites.

--- apps/marketing-site/vite.config.ts
+++ apps/marketing-site/vite.config.ts
@@ -2,5 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  // Required for GitHub Pages project site: https://pythonidaer.github.io/design-system/
+  base: '/design-system/',
   plugins: [react()],
 });
